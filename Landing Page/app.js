4c91a78dd927d57424f0642a34cd191b150fb4aa
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-05-06 01:52:16 +0530

    updated

--- Landing Page/app.js
+++ Landing Page/app.js
@@ -1,8 +1,8 @@
-document.addEventListener('click', function (event) {
-    var x = event.clientX;
-    var y = event.clientY;
-    console.log('Mouse click position: ' + x + ', ' + y);
-});
+// document.addEventListener('click', function (event) {
+//     var x = event.clientX;
+//     var y = event.clientY;
+//     console.log('Mouse click position: ' + x + ', ' + y);
+// });
 
 const blinkSquare = document.querySelector('#blink-square');
 const blinkSpacebar = document.querySelector('#blink-spacebar');
@@ -11,35 +11,35 @@ blinkSquare.addEventListener('animationend', handleAnimationEnd);
 
 function handleAnimationEnd() {
     restartAnimation(541, 434); //e
-    setTimeout(() => restartAnimation(472, 474), 107); //a
-    setTimeout(() => restartAnimation(581, 434), 214); //r
-    setTimeout(() => restartAnimation(690, 513.5), 321); //n
+    setTimeout(() => restartAnimation(472, 474), 178.57); //a
+    setTimeout(() => restartAnimation(581, 434), 357.14); //r
+    setTimeout(() => restartAnimation(690, 513.5), 535.71); //n
 
-    setTimeout(() => restartAnimation(620.25, 434), 535); //t
-    setTimeout(() => restartAnimation(780, 434), 642); //o
-    setTimeout(() => restartAnimation(700.25, 434), 749); //u
-    setTimeout(() => restartAnimation(570.5, 513.5), 856); //c
-    setTimeout(() => restartAnimation(670.5, 474), 963); //h
-    setTimeout(() => spacebarBlink(), 1070); //spacebar
+    setTimeout(() => restartAnimation(620.25, 434), 892.85); //t
+    setTimeout(() => restartAnimation(780, 434), 1071.42); //o
+    setTimeout(() => restartAnimation(700.25, 434), 1249.99); //u
+    setTimeout(() => restartAnimation(570.5, 513.5), 1428.56); //c
+    setTimeout(() => restartAnimation(670.5, 474), 1607.13); //h
+    setTimeout(() => spacebarBlink(), 1785.7); //spacebar
 
-    setTimeout(() => restartAnimation(620.25, 434), 1177); //t
-    setTimeout(() => restartAnimation(660.4, 434), 1284); //y
-    setTimeout(() => restartAnimation(819.7, 434), 1391); //p
-    setTimeout(() => restartAnimation(740, 434), 1498); //i
-    setTimeout(() => restartAnimation(690, 513.5), 1605); //n
-    setTimeout(() => restartAnimation(630, 474), 1712); //g
-    setTimeout(() => spacebarBlink(), 1819); //spacebar
+    setTimeout(() => restartAnimation(620.25, 434), 1964.27); //t
+    setTimeout(() => restartAnimation(660.4, 434), 2142.84); //y
+    setTimeout(() => restartAnimation(819.7, 434), 2321.41); //p
+    setTimeout(() => restartAnimation(740, 434), 2499.98); //i
+    setTimeout(() => restartAnimation(690, 513.5), 2678.55); //n
+    setTimeout(() => restartAnimation(630, 474), 2857.12); //g
+    setTimeout(() => spacebarBlink(), 3035.69); //spacebar
 
-    setTimeout(() => restartAnimation(591, 474), 1926); //f
-    setTimeout(() => restartAnimation(780, 434), 2033); //o
-    setTimeout(() => restartAnimation(581, 434), 2140); //r
-    setTimeout(() => spacebarBlink(), 2247); //spacebar
+    setTimeout(() => restartAnimation(591, 474), 3214.26); //f
+    setTimeout(() => restartAnimation(780, 434), 3392.83); //o
+    setTimeout(() => restartAnimation(581, 434), 3571.4); //r
+    setTimeout(() => spacebarBlink(), 3749.97); //spacebar
 
-    setTimeout(() => restartAnimation(591, 474), 2354); //f
-    setTimeout(() => restartAnimation(580, 434), 2461); //r
-    setTimeout(() => restartAnimation(541, 434), 2578); //e
-    setTimeout(() => restartAnimation(541, 434), 2685); //e
-    setTimeout(() => restartAnimation(809.5, 513.5), 2792); //.
+    setTimeout(() => restartAnimation(591, 474), 3928.54); //f
+    setTimeout(() => restartAnimation(580, 434), 4107.11); //r
+    setTimeout(() => restartAnimation(541, 434), 4285.68); //e
+    setTimeout(() => restartAnimation(541, 434), 4464.25); //e
+    setTimeout(() => restartAnimation(809.5, 513.5), 4642.82); //.
 
     // remove event listener after it runs for the first time
     blinkSquare.removeEventListener('animationend', handleAnimationEnd);
@@ -52,12 +52,12 @@ function restartAnimation(x, y) {
     // blinkSquare.classList.remove('blinked');
     blinkSquare.style.animation = 'none';
     void blinkSquare.offsetWidth;
-    blinkSquare.style.animation = 'blinked 107ms ease forwards';
+    blinkSquare.style.animation = 'blinked 178.57ms ease forwards';
     // blinkSquare.classList.add('blinked');
 }
 
 function spacebarBlink(){
     blinkSpacebar.style.animation = 'none';
     void blinkSpacebar.offsetWidth;
-    blinkSpacebar.style.animation = 'blinked 107ms forwards';
+    blinkSpacebar.style.animation = 'blinked 178.57ms forwards';
 }
